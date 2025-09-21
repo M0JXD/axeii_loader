038dacc08fbc1f9858ce0d8ab409a7293c9973e9
M0JXD <jamiedrinkell@gmail.com>
2025-09-21 22:21:01 +0100

    transfer settings in one file as getter is always same

--- lib/widgets/transfer_settings.dart
+++ lib/widgets/transfer_settings.dart
@@ -1,6 +1,5 @@
-import 'package:axeii_loader/widgets/ir_settings.dart';
-import 'package:axeii_loader/widgets/preset_settings.dart';
 import 'package:flutter/material.dart';
+import 'package:input_quantity/input_quantity.dart';
 import 'package:provider/provider.dart';
 import 'package:axeii_loader/model/model.dart';
 
@@ -14,20 +13,23 @@ class TransferSettings extends StatelessWidget {
     AxeFileType? type;
     var mode = context.watch<AxeLoaderViewModel>().sendReceiveMode;
     if (context.watch<AxeLoaderViewModel>().fileLocation != null) {
-      type = context.watch<AxeLoaderViewModel>().fileType;
-      switch (type) {
-        case null:
-          information = Text("File type unknown.");
-        case AxeFileType.ir:
-          information = Text("IR File Detected.");
-          controls = IRSettings();
-        case AxeFileType.preset:
-          information = Text("Preset File Detected.");
-          if (context.read<AxeLoaderViewModel>().sendReason.isNotEmpty) {
-            controls = Text(context.watch<AxeLoaderViewModel>().sendReason);
-          } else if (mode == SendReceiveMode.receive) {
-            controls = GetPresetSettings();
-          }
+      if (mode == SendReceiveMode.send) {
+        type = context.watch<AxeLoaderViewModel>().fileType;
+        switch (type) {
+          case null:
+            information = Text("File type unknown.");
+          case AxeFileType.ir:
+            information = Text("IR File Detected.");
+            controls = IRSettings();
+          case AxeFileType.preset:
+            information = Text("Preset File Detected.");
+            if (context.read<AxeLoaderViewModel>().sendReason.isNotEmpty) {
+              information = Text("Preset File Detected.\n${context.read<AxeLoaderViewModel>().sendReason}");
+            }
+        }
+      } else {
+        information = Text("Select what to receive:");
+        controls = GetterSettings();
       }
     } else {
       if (mode == SendReceiveMode.send) {
@@ -48,3 +50,77 @@ class TransferSettings extends StatelessWidget {
           );
   }
 }
+
+class GetterSettings extends StatelessWidget {
+  const GetterSettings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Row(
+          spacing: 10,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Preset Number to Get:"),
+            InputQty(
+              decoration: QtyDecorationProps(
+                qtyStyle: QtyStyle.btnOnRight,
+                btnColor: Colors.grey,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Theme.of(context).hintColor),
+                ),
+              ),
+            ),
+
+
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+
+class IRSettings extends StatelessWidget {
+  const IRSettings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("IR Settings:"),
+        Row(
+          children: [
+            RadioGroup<CabLocation>(
+              groupValue: context.watch<AxeLoaderViewModel>().cabLocation,
+              onChanged: (CabLocation? value) {
+                context.read<AxeLoaderViewModel>().cabLocation = value!;
+              },
+              child: SizedBox(
+                width: 170,
+                child: const Column(
+                  children: <Widget>[
+                    RadioListTile<CabLocation>(
+                      value: CabLocation.user,
+                      title: Text("User"),
+                    ),
+                    RadioListTile<CabLocation>(
+                      value: CabLocation.scratchpad,
+                      title: Text("Scratchpad"),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            InputQty(),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+
