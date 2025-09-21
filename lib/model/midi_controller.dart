8e493f18ffccc3b986ac2bc54f086c7cdc1f36b9
M0JXD <jamiedrinkell@gmail.com>
2025-09-21 21:38:54 +0100

    Add draft upload cab implementation

--- lib/model/midi_controller.dart
+++ lib/model/midi_controller.dart
@@ -11,12 +11,19 @@ class AxeController {
   static List<int> currentSysex = [];
   MidiDevice device;
   AxeFXType axeFXType;
-  AxeController({required this.device, this.axeFXType = AxeFXType.original});
+  AxeFXType fileUnit;
+  String location;
+
+  AxeController({
+    required this.device,
+    required this.location,
+    required this.fileUnit,
+    this.axeFXType = AxeFXType.original,
+  });
 
   //----- Methods -----//
   Uint8List recalcSysex(Uint8List sysex) {
     int checksumByte = 0xF0;
-    int len = sysex.length;
     switch (axeFXType) {
       case AxeFXType.xl:
         sysex[4] = 0x06;
@@ -32,40 +39,36 @@ class AxeController {
     }
 
     // Add checksum and sysex end byte
-    for (int i = 1; i < (len - 2); i++) {
+    for (int i = 1; i < (sysex.length - 2); i++) {
       checksumByte ^= sysex[i];
     }
     checksumByte &= 0x7F;
-    sysex[len - 2] = checksumByte;
-    sysex[len - 1] = 0xF7;
+    sysex[sysex.length - 2] = checksumByte;
+    sysex[sysex.length - 1] = 0xF7;
     return sysex;
   }
 
-  AxeFXType fileUnitType(String fileName) {
-    return AxeFXType.original;
-  }
-
-  Stream<double> uploadPreset(String pathToPreset) async* {
+  Stream<double> uploadPreset() async* {
     final dataPackets = axeFXType == AxeFXType.original ? 32 : 64;
-    var fileToSend = (await File(pathToPreset).readAsBytes());
-    var fileBytes = fileToSend.length;
+    var fileToSend = (await File(location).readAsBytes());
+    final fileBytes = fileToSend.length;
     await MidiCommand().connectToDevice(device);
 
     Uint8List startSysex = fileToSend.sublist(0, 12);
     startSysex = recalcSysex(startSysex);
     MidiCommand().sendData(startSysex);
     yield (1.0 / dataPackets + 2);
-    await Future.delayed(const Duration(milliseconds: 10));
+    await Future.delayed(const Duration(milliseconds: 5));
 
     for (var i = 0; i < dataPackets; i++) {
       Uint8List sysexToSend = fileToSend.sublist(
         12 + (202 * i),
-        214 + (202 * i),
+        12 + (202 * (i + 1)),
       );
       sysexToSend = recalcSysex(sysexToSend);
       MidiCommand().sendData(sysexToSend);
       yield (i + 1) / (dataPackets + 2);
-      await Future.delayed(const Duration(milliseconds: 10));
+      await Future.delayed(const Duration(milliseconds: 5));
     }
 
     Uint8List endSysex = fileToSend.sublist(fileBytes - 11);
@@ -76,7 +79,7 @@ class AxeController {
     MidiCommand().disconnectDevice(device);
   }
 
-  Stream<double> downloadPreset(String pathToSave) async* {
+  Stream<double> downloadPreset() async* {
     Uint8List fileData = Uint8List(12951);
     MidiCommand().connectToDevice(device);
 
@@ -85,23 +88,51 @@ class AxeController {
       // Check that this is the end of the file...
       fileData.replaceRange(i, value.data.length, value.data);
       if (i % 100 == 0) {
-        yield 1 / 66 * i;
+        yield 1.0 / 66 * i;
       }
       i++;
     }
     yield 1;
     MidiCommand().disconnectDevice(device);
 
-    var file = File(pathToSave);
+    var file = File(location);
     file.create();
   }
 
-  Stream<double> uploadCab(String pathToCab, int location) async* {}
+  Stream<double> uploadCab() async* {
+    var fileToSend = (await File(location).readAsBytes());
+    var fileBytes = fileToSend.length;
+    final dataStartAdd = fileUnit == AxeFXType.original ? 11 : 12;
+    await MidiCommand().connectToDevice(device);
 
-  Stream<double> downloadCab(String pathToSave, int location) async* {}
+    Uint8List startSysex = fileToSend.sublist(0, 12);
+    startSysex = recalcSysex(startSysex);
+    MidiCommand().sendData(startSysex);
+    yield (1.0 / 64 + 2);
+    await Future.delayed(const Duration(milliseconds: 5));
+
+    for (var i = 0; i < 64; i++) {
+      Uint8List sysexToSend = fileToSend.sublist(
+        dataStartAdd + (170 * i),
+        dataStartAdd + (170 * (i + 1)),
+      );
+      sysexToSend = recalcSysex(sysexToSend);
+      MidiCommand().sendData(sysexToSend);
+      yield (i + 1) / (64 + 2);
+      await Future.delayed(const Duration(milliseconds: 5));
+    }
+
+    Uint8List endSysex = fileToSend.sublist(fileBytes - 13);
+    endSysex = recalcSysex(endSysex);
+    MidiCommand().sendData(endSysex);
+    yield 1.0;
+
+    MidiCommand().disconnectDevice(device);
+  }
+
+  Stream<double> downloadCab() async* {}
 
   //----- Utility -----//
-
   static void devChecker() async {
     print("Checking MIDI devices...");
     var devices = await MidiCommand().devices;
